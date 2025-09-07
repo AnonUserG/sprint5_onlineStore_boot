0909d2830379af693c583bd1763d1cfcf19e7632
AnonUserG <nick1029384@gmail.com>
2025-09-07 15:27:33 +0300

    delete no usage

--- src/main/java/ru/practicum/onlineStore/repository/OrderItemRepository.java
+++
@@ -1,9 +0,0 @@
-package ru.practicum.onlineStore.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import ru.practicum.onlineStore.model.OrderItem;
-
-@Repository
-public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
-}
